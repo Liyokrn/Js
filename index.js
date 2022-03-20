d0d75502a38c1f9006ca70ca725002b335b9241d
LiyoKrn <ccortes14@ucol.mx>
2022-03-19 21:48:34 -0600

    second commit

---
+++ index.js
@@ -0,0 +1,42 @@
+/*let num1= parseInt(prompt("introduce un numero"));
+let num2= parseInt(prompt("dame otro"));   
+
+function sumar(num1, num2) {   
+    let resultado= num1+num2;
+    return
+}
+
+
+const suma=(num1, num2)=> resultado=num1 + num2;
+
+let res= suma(num1, num2);
+document.write("El resultado es:"+ res);
+
+//aun que no considero correcto poner las etiquetas html dentro del objeto lo hare con el fin de facilitar la captura de evidencia
+
+const imprimirFrase= nombre=> ` <br>  Hola ${nombre} </br>`;
+
+let nombre= '<br> juancharlie </br>';
+
+document.write( imprimirFrase(nombre ));
+
+
+const carro={
+    color: 'negro',
+    marca: 'la toyotona',
+    funciones:{
+        acelera: 160, 
+        frena: 0
+
+    }
+}
+
+console.log( color);
+console.log(carro.funciones);
+console.log(carro);
+
+const {colorCarro}= carro;
+
+const {color} =carro;
+console.log(color);  */
+
